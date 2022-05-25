ecb6f3a998e707134639f7f6a4831be9b97fd67f
Jaeden West <jaeden.t.west@gmail.com>
2022-05-24 19:08:24 -0600

    character counter changes to red upon surpasing the character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   $('textarea').on('input', function () {
-    console.log(this);
     const characters = $(this).val();
     const count = 140 - characters.length;
     const upDOM = $(this).closest('form');
-    const downDOM = $(upDOM).find('output');
-    $(downDOM).val(count);
+    const counter = $(upDOM).find('output');
+    $(counter).val(count);
+    //style red
+    if(count <= -1) {
+      $(counter).toggleClass('red', true);
+    } else {
+      $(counter).toggleClass('red', false);
+    }
   });
 });
+
+//I know the style should be done in CSS but this is the least complicated and most effective way to do the color change on the character counter.
