bc0355a65b2c44eb75463e5dba9d5146e4961ea0
Jaeden West <jaeden.t.west@gmail.com>
2022-05-28 11:49:10 -0600

    submit tweet handling in progress, submit lister implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = function(tweet) {
   const $tweet = $(`
   <article class="tweet hover-shadow">
@@ -65,5 +40,20 @@ const renderTweets = function(arrTweets) {
   }
 };
 
-renderTweets(data);
+$(document).ready(
+  $('form').submit(function(e) {
+    console.log('something');
+    e.preventDefault();
+    serialEvent = $(this).serialize();
+    $.post("/tweets/", serialEvent, () => { 
+      console.log(serialEvent);
+    })
+  })
+)
+
+$(document).ready(
+  function loadTweets() {
+    $.get("/tweets/",)
+  }
+)
 
